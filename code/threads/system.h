5883f4fa7f9a582d76d964f3c986a898cde40cd3
Plinz <an.duquennoy@gmail.com>
2017-12-12 16:40:40 +0100

    Partie V : ajout de fichiers test putchar.c et putstring.c | ajout des appel sytemes PutChar et SynchPutChar dans syscall.h avec leur code assembleur dans start.S | ajout des handlers pour les interuption putchar et sycnputchar dans exception.cc | progtest -> fix error duplicate synchconsole | system.cc -> création d'une synchconsole à l'initialisation et ajout des argument -i et -o respectivement pour les fichier in et out | system.h -> creation const MAX_STRING_SIZE et ajout de synchconsole

--- code/threads/system.h
+++ code/threads/system.h
@@ -1,8 +1,8 @@
-// system.h 
+// system.h
 //      All global variables used in Nachos are defined here.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #ifndef SYSTEM_H
@@ -30,8 +30,11 @@ extern Statistics *stats;	// performance metrics
 extern Timer *timer;		// the hardware alarm clock
 
 #ifdef USER_PROGRAM
+#define MAX_STRING_SIZE 1000
 #include "machine.h"
+#include "synchconsole.h"
 extern Machine *machine;	// user program memory and registers
+extern SynchConsole *synchconsole;
 #endif
 
 #ifdef FILESYS_NEEDED		// FILESYS or FILESYS_STUB
--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //      Entry point into the Nachos kernel from user programs.
 //      There are two kinds of things that can cause control to
 //      transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //      exceptions -- The user code does something that the CPU can't handle.
 //      For instance, accessing memory that doesn't exist, arithmetic errors,
-//      etc.  
+//      etc.
 //
 //      Interrupts (which can also cause control to transfer from user
 //      code into the Nachos kernel) are handled elsewhere.
@@ -18,20 +18,28 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
 #include "system.h"
 #include "syscall.h"
 
+void copyStringFromMachine(int from, char *to, unsigned size){
+    unsigned int i=0;
+    int c;
+    while(i<(size-1) && machine->ReadMem(from+i, 1, &c) && (char)c != '\0') {
+        *(to+i) = (char)c;
+        i++;
+    }
+    *(to+i) = '\0';
+}
+
 //----------------------------------------------------------------------
 // UpdatePC : Increments the Program Counter register in order to resume
 // the user program immediately after the "syscall" instruction.
 //----------------------------------------------------------------------
-static void
-UpdatePC ()
-{
+static void UpdatePC (){
     int pc = machine->ReadRegister (PCReg);
     machine->WriteRegister (PrevPCReg, pc);
     pc = machine->ReadRegister (NextPCReg);
@@ -55,32 +63,43 @@ UpdatePC ()
 //              arg3 -- r6
 //              arg4 -- r7
 //
-//      The result of the system call, if any, must be put back into r2. 
+//      The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//      "which" is the kind of exception.  The list of possible exceptions 
+//      "which" is the kind of exception.  The list of possible exceptions
 //      are in machine.h.
 //----------------------------------------------------------------------
 
-void
-ExceptionHandler (ExceptionType which)
-{
-    int type = machine->ReadRegister (2);
+void ExceptionHandler (ExceptionType which){
+    int type, reg4;
+    type = machine->ReadRegister (2);
+    reg4 = machine->ReadRegister (4);
 
-    if ((which == SyscallException) && (type == SC_Halt))
-      {
-	  DEBUG ('a', "Shutdown, initiated by user program.\n");
-	  interrupt->Halt ();
-      }
-    else
-      {
-	  printf ("Unexpected user mode exception %d %d\n", which, type);
-	  ASSERT (FALSE);
-      }
+    if (which == SyscallException) {
+        switch (type) {
+            case SC_Halt:
+                DEBUG('a', "Shutdown, initiated by user program.\n");
+                interrupt->Halt();
+                break;
+            case SC_PutChar:
+                DEBUG('a', "PutChar, initiated by user program.\n");
+                synchconsole->SynchPutChar(reg4);
+                break;
+            case SC_SynchPutString:
+                DEBUG('a', "PutChar, initiated by user program.\n");
+                char buf [MAX_STRING_SIZE];
+                copyStringFromMachine(reg4, buf, MAX_STRING_SIZE);
+                synchconsole->SynchPutString(buf);
+                break;
+            default:
+                printf("Unexpected user mode exception %d %d\n", which, type);
+                ASSERT(FALSE);
+        }
+    }
 
     // LB: Do not forget to increment the pc before returning!
-    UpdatePC ();
+    UpdatePC();
     // End of addition
 }
