db94574e16663566a7039a4de2739e0cd9e7d7ce
Plinz <an.duquennoy@gmail.com>
2017-12-14 21:15:24 +0100

    StartUserThread : Mise a jour du PC pour pointer vers la fonction et save des arg dans le registre 4, passage de l'adresse de la fonction par argument de la fonction Fork et passage de arg par le registre HiReg (pas sur d'avoir le droit :/)

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -5,6 +5,10 @@ static void StartUserThread(int f){
     //On doit récupérer les deux arguments uniquement a l'aide de int f !
     //Placer le registre du pointeur sur la fonction f (la mettre en memoire ?) et mettre le(s) arguments (a mettre en memoire ?) dans les registres 4/5/6/7 ...
     //Puis lancer la fonction (machine->Run()) voir dans progtest StartProcess ce qui est fait je pense que c'est similaire
+    machine->WriteRegister (PrevPCReg, machine->ReadRegister (PCReg));
+    machine->WriteRegister (PCReg, f);
+    machine->WriteRegister (NextPCReg, f+4);
+    machine->WriteRegister (4, machine->ReadRegister(HiReg));
 
     printf("startuserthread not implemented yet\n");
 }
@@ -12,7 +16,8 @@ static void StartUserThread(int f){
 int do_UserThreadCreate(int f, int arg){
     Thread *t;
     if((t = new Thread ("User_Thread")) == NULL) {return -1;}
-    t->Fork(StartUserThread,arg);
+    machine->WriteRegister(HiReg, arg);
+    t->Fork(StartUserThread,f);
     return 0;
 }
 
