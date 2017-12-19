9913843cba84573e020b0acb6b6a78320b5d3a40
TheSpyGeek <mathieurire@gmail.com>
2017-12-19 11:18:02 +0100

    changement de test PutInt

--- code/test/test_putInt.c
+++ code/test/test_putInt.c
@@ -3,6 +3,9 @@
 
 // test PutInt avec passage de variables dans une fonction
 void printInt(int test){
+	int i = 27;
+	PutInt(i);
+	PutChar('\n');
 	PutInt(test);
 	PutChar('\n');
 }
@@ -10,12 +13,15 @@ void printInt(int test){
 
 int main(){
 
-	int i = 5;
+	int i = 35;
 
 	PutInt(10);
 	PutChar('\n');
 
 	PutInt(202000);
+	PutChar('\n');
+
+	PutInt(i);
 	PutChar('\n');
 
 	printInt(200);
