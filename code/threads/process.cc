683ba9bcaba33cfbc43b29e40decec67b54a30f7
bornej <bornej@im2ag-mandelbrot.e.ujf-grenoble.fr>
2018-01-11 13:11:50 +0100

    documentation process.h

--- code/threads/process.cc
+++ code/threads/process.cc
@@ -3,14 +3,12 @@
 
 Process::Process(const char *pName) {
     pid = ++processCounter;
-
     if(currentThread == NULL) {
         ppid = pid; /* Le processus courant est le processus init */
     } else {
         ppid = currentThread->getPid();
     }
     processName = pName;
-
     launcherThread = new Thread(processName);
     launcherThread->setPid(pid);
     threadList = new std::map<int, Thread*>();
@@ -18,15 +16,10 @@ Process::Process(const char *pName) {
 }
 
 Process::~Process(){
-
-
-
     delete threadList;
 }
 
-
 void Process::startProcess(char * fileName){
-
     OpenFile *executable = fileSystem->Open(fileName);
     AddrSpace *space;
 
@@ -34,42 +27,42 @@ void Process::startProcess(char * fileName){
         printf ("Unable to open file %s\n", fileName);
         ASSERT(FALSE);
     }
-
     space = new AddrSpace (executable);
     launcherThread->space = space;
-
     delete executable;      // close file
-
     space->InitRegisters ();    // set the initial register values
     space->RestoreState (); // load page table register
-
+    // ReadyToRun assumes that interrupts
+    // are disabled!
     IntStatus oldLevel = interrupt->SetLevel (IntOff);
-
     if(launcherThread->getStatus() != RUNNING) {
-        scheduler->ReadyToRun(launcherThread);  // ReadyToRun assumes that interrupts
-        // are disabled!
+        /* Si le thread propulseur est le thread courrant on ne l'ajoute
+           pas à la ready list.
+           Cette situation se produit lors de la création du premier
+           processus utilisateur dans system.cc.
+           Suite à la création du processus on branche le scheduler
+           "à la main" sur le thread propulseur.
+        */
+        scheduler->ReadyToRun(launcherThread);
     }
     (void) interrupt->SetLevel (oldLevel);
-
 }
 
 void Process::finish(){
-
     for(std::map<int,Thread*>::iterator it=threadList->begin() ; it!=threadList->end() ; ++it){
-        // on détruit tous les thread de la liste sauf le thread courant qui sera détruit par le scheduler
+        /* on détruit tous les thread de la liste sauf le thread courant qui doit être détruit par la methode finish (on travaille encore dans la pile d'éxécution de ce processus) -> threadTobedestroyed */
         if (it->second == currentThread){
-            it->second->Finish(); // accede à la valeur
+            it->second->Finish(); /* it->second pour accèder à la valeur */
         } else {
             it->second->setStatus(TERMINATED);
         }
     }
-    /* Suppréssion des threads TERMINATED de la ready list du scheduler */
-    IntStatus oldLevel = interrupt->SetLevel (IntOff);
-    scheduler->RemoveThreadFromReadyList();
-    (void) interrupt->SetLevel (oldLevel);
-
+    /* Suppression des threads TERMINATED de la ready list du scheduler */
+    // Pas utilisé
+    //IntStatus oldLevel = interrupt->SetLevel (IntOff);
+    //scheduler->RemoveThreadFromReadyList();
+    //(void) interrupt->SetLevel (oldLevel);
     /* TODO mettre à jour la bitmap */
-
 }
 
 void Process::addThread(Thread * newThread){
