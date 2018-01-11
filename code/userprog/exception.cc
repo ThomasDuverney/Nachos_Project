031d2e9978e6e5a346a3a455269ffd547661614c
thomasduverney <thomas.duverney@etu.univ-grenoble-alpes.com>
2018-01-11 19:47:34 +0100

    ajout de currentprocessus->finish() dans le case Sc_Exit()

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -152,16 +152,15 @@ void ExceptionHandler (ExceptionType which){
                 break;
             case SC_Exit:
                 /*
-                  La valeur de retour du main ou exit est dans le
-                  registre 4
-                  Remarque: synchconsole->Flush();
-                  Pour des raisons de sécurité on flush la
+                  La valeur de retour du main ou exit est dans le registre 4
+                  Remarque: synchconsole->Flush(); Pour des raisons de sécurité on flush la
                   la sortie standard??
                 */
                 //currentProcess->finish();
                 DEBUG('a', "Exit, initiated by user program.\n");
                 break;
             case SC_ForkExec:
+                DEBUG('a', "Syscall ForkExec");
                 char executableName [MAX_STRING_SIZE];
                 copyStringFromMachine(reg4, executableName, MAX_STRING_SIZE);
                 Process * newProcess;
