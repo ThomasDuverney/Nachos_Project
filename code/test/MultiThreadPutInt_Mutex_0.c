3d42ccf5fc48fe376d6152c39817a55ea735f6c2
bornej <bornej@im2ag-mandelbrot.e.ujf-grenoble.fr>
2018-01-14 18:00:33 +0100

    Programmes de Tests Multithread Mutex

---
+++ code/test/MultiThreadPutInt_Mutex_0.c
@@ -0,0 +1,36 @@
+#include "syscall.h"
+
+#define NB 5
+
+/*
+ * Création de plusieurs threads exécutant putInt.
+ * On vérifie le bon fonctionnement des structures de synchronisation par
+ * l'utilisation concurrente de synchconsole
+ */
+
+unsigned int mutex;
+
+void g(void *arg) {
+  MutexLock(mutex);
+  PutInt(*(int *)arg);
+  MutexUnlock(mutex);
+  UserThreadExit();
+}
+
+
+int main(){
+  int tab[NB];
+  int i;
+  int tid[NB];
+  mutex = MutexCreate();
+  for(i=0; i<NB; i++){
+    tab[i] = i;
+    tid[i] = UserThreadCreate(g,(void*) (tab+i));
+  }
+
+  for(i=0; i<NB; i++){
+    UserThreadJoin(tid[i]);
+  }
+  MutexDestroy(mutex);
+  return 0;
+}
